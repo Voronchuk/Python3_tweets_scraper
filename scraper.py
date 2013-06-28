a3152a4d5b4fe2ad4e3a7f0e64dabf8e122f0965
Vyacheslav Voronchuk <voronchuk@gmail.com>
2013-06-28 22:59:45 +0300

    Update scraper.py
    Formating

--- scraper.py
+++ scraper.py
@@ -1,10 +1,12 @@
 import csv, ast
+
 with open("linkWD.csv") as csvfile:
     userreader = csv.reader(csvfile, delimiter=';')
     reweightedLinks = {}
     Weight = {}
     maxWeigthTolerance = 2
     maxWeigth = 0
+    
     for row in userreader:
         link = row[0]
         wd = row[1]
@@ -13,6 +15,7 @@ with open("linkWD.csv") as csvfile:
         if (maxWeigth < W):
             maxWeigth = W
         Weight[link] = W
+        
 BigWeight = {}
 levelOfFiltration = maxWeigth - maxWeigthTolerance
 if (levelOfFiltration) < 2:
@@ -21,4 +24,5 @@ for link in Weight:
     w = Weight[link]
     if w >= levelOfFiltration:
         BigWeight[link] = w
+        
 print(BigWeight)
